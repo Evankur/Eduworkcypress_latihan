f48851334fb389fc73e4f22c0d84f67887cc85c5
Evan <Evankurniawan76@Gmail.com>
2024-07-23 13:06:58 +0700

    checklisttest

--- inputtest.cy.js
+++ inputtest.cy.js
@@ -4,22 +4,14 @@ describe('working inputs', () => {
         cy.url().should('include','login.html')
     });
     it('should isi username',()=>{
-        cy.visit('http://zero.webappsecurity.com/login.html',{timeout : 10000})
         cy.get('#user_login').clear()
         cy.get('#user_login').type('username')
     });
     it('should isi password',()=>{
-        cy.visit('http://zero.webappsecurity.com/login.html',{timeout : 10000})
-        cy.get('input[name="user_password"]').clear()
-        cy.get('input[name="user_password"]').type('password')
-    });
-    it('should isi password',()=>{
-        cy.visit('http://zero.webappsecurity.com/login.html',{timeout : 10000})
         cy.get('input[name="user_password"]').clear()
         cy.get('input[name="user_password"]').type('password')
     });
     it('should checklist',()=>{
-        cy.visit('http://zero.webappsecurity.com/login.html',{timeout : 10000})
         cy.get('[type="checkbox"]').check()
         cy.get('[type="checkbox"]').first().check()
     });
